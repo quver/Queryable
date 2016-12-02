b4b7ed32fa4d4f6efbeb3daf81b9cace8a348762
Paweł Bednorz <pawel@quver.pl>
2016-12-02 18:20:00 +0100

    [QT-2] Project structure change

---
+++ Queryable/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Queryable
+//
+//  Created by Pawel Bednorz on 02.12.2016.
+//  Copyright © 2016 Quver.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
