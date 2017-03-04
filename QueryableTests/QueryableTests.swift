fe8a097debb6c17f525569892564bc6498d2bc78
Paweł Bednorz <pawel@quver.pl>
2017-03-04 17:52:08 +0100

    [QT-10] Add results property + refactor

--- QueryableTests/QueryableTests.swift
+++ QueryableTests/QueryableTests.swift
@@ -46,80 +46,97 @@ class QueryableTests: QuickSpec {
 
   override func spec() {
     beforeEach {
-      _ = QueryManager.removeAll()
+      QueryManager.removeAll()
     }
 
     context("add object") {
       it("is containing it") {
         let someObject = self.someObject()
-        _ = someObject.add()
+        someObject.add()
 
-        expect(TestEntity.allObjects()).to(contain(someObject))
+        expect(TestEntity.arrayOfObject).to(contain(someObject))
       }
 
       it("is not containing other") {
         let otherObject = self.otherObject()
         let someObject = self.someObject()
-        _ = someObject.add()
+        someObject.add()
 
-        expect(TestEntity.allObjects()).notTo(contain(otherObject))
+        expect(TestEntity.arrayOfObject).notTo(contain(otherObject))
       }
 
       it("has only one object") {
         let someObject = self.someObject()
-        _ = someObject.add()
+        someObject.add()
 
-        expect(TestEntity.allObjects().count).to(equal(1))
+        expect(TestEntity.arrayOfObject).to(haveCount(1))
       }
     }
 
     context("edit object") {
       it("change property") {
         let someObject = self.someObject()
-        _ = someObject.add()
-        _ = someObject.update(transaction: {
+        someObject.add()
+        someObject.update(transaction: {
           someObject.text = self.otherString
           }, completion: nil)
 
-        expect(TestEntity.allObjects().first!.text).to(equal(self.otherString))
+        expect(TestEntity.arrayOfObject.first!.text).to(equal(self.otherString))
       }
 
       it("has only one object") {
         let someObject = self.someObject()
-        _ = someObject.add()
-        _ = someObject.update(transaction: {
+        someObject.add()
+        someObject.update(transaction: {
           someObject.text = self.otherString
           }, completion: nil)
 
-        expect(TestEntity.allObjects().count).to(equal(1))
+        expect(TestEntity.arrayOfObject).to(haveCount(1))
       }
     }
 
     context("remove object") {
       it("one left") {
         let someObject = self.someObject()
-        _ = someObject.add()
+        someObject.add()
 
         let otherObject = self.otherObject()
-        _ = otherObject.add()
+        otherObject.add()
 
-        _ = someObject.remove()
+        someObject.remove()
 
-        expect(TestEntity.allObjects()).to(contain(otherObject))
+        expect(TestEntity.arrayOfObject).to(contain(otherObject))
       }
     }
 
     context("get objects") {
-      it("filter object") {
-        let someObject = self.someObject()
-        _ = someObject.add()
+      beforeEach {
+        self.someObject().add()
+        self.otherObject().add()
+      }
 
-        let otherObject = self.otherObject()
-        _ = otherObject.add()
+      context("to array") {
+        it("all objects") {
+          expect(TestEntity.arrayOfObject).to(haveCount(2))
+        }
 
-        let filtered = TestEntity.filtred("text == '\(self.otherString)'")
+        it("with filter") {
+          let filtered = TestEntity.filtredArray("text == '\(self.otherString)'")
 
-        expect(filtered.first).to(equal(otherObject))
+          expect(filtered.first!.text).to(equal(self.otherString))
+        }
+      }
+
+      context("to result") {
+        it("all objects") {
+          expect(TestEntity.resultsOfObject).to(haveCount(2))
+        }
+
+        it("with filter") {
+          let filtered = TestEntity.filtredResults("text == '\(self.otherString)'")
+
+          expect(filtered.first!.text).to(equal(self.otherString))
+        }
       }
     }
   }
